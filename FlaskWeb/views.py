85649f46606c3ab3b619c4a2f120100849d4463a
Ketaki Kumbharkar <k.ketaki28@gmail.com>
2019-07-21 20:36:59 -0400

    Update views.py

--- FlaskWeb/views.py
+++ FlaskWeb/views.py
@@ -1,88 +1,37 @@
-from flask import Flask, request, flash, url_for, redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
+"""
+Routes and views for the flask application.
+"""
 
-app = Flask(__name__)
-#basedir = os.path.abspath(os.path.dirname(__file__))
-#dburi = 'sqlite:///' + os.path.join(basedir, 'data/app.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employee.sqlite3'
-app.config['SECRET_KEY'] = "random string"
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['SQLALCHEMY_ECHO'] = False
-
-db = SQLAlchemy(app)
-
-class Employee(db.Model):
-    emp_id = db.Column(db.String(20), primary_key=True)
-    name = db.Column(db.String(30))
-    email = db.Column(db.String(80))
-    password = db.Column(db.String(20))
-
-    def __init__(self, emp_id,name, email, password):
-        self.emp_id=emp_id
-        self.name = name
-        self.email = email
-        self.password = password
+from datetime import datetime
+from flask import render_template
+from FlaskWeb import app
 
 @app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
 
-@app.route('/register', methods=['GET','POST'])
-def register():
-    try:
-        if request.method == 'POST':
-            if not request.form['emp_id'] or not request.form['name'] or not request.form['email'] or not request.form['password']:
-                flash('Please enter all the fields', 'error')
-            else:
-                if request.form['password'] == request.form['password_confirm']:
-                    emp = Employee(request.form['emp_id'], request.form['name'], request.form['email'], request.form['password'])
-                    db.session.add(emp)
-                    db.session.commit()
-                    flash('Record was successfully added')
-                else:
-                    flash('Password Mismatch!!')
-    except BaseException as e:
-            print(e)
-
-    return render_template('register.html')
-
-
-@app.route('/login', methods=['GET','POST'])
-def login():
-    try:
-        if request.method == 'POST':
-            if not request.form['emp_id'] or not request.form['password']:
-                flash('Please enter all the fields', 'error')
-            else:
-                emp_id= request.form['emp_id']
-                emp = Employee.query.filter_by(emp_id=emp_id).first()
-                if request.form['password'] == emp.password:
-                    flash('Login Successful')
-                    return render_template('home.html')
-                else:
-                    flash('Emaployee id or password mismatch')
-
-    except BaseException as e:
-            print(e)
-
-    return render_template('login.html')
-
-
-
-
-@app.route('/vote')
-def vote():
-    return render_template('vote.html')
-
-
-if __name__=='__main__':
-    db.create_all()
-    app.run(debug=True)
-
-
-
-
-
-
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
